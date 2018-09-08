87758ed62495024be73843c9fa4085b2ac8f0a32
Jaineshkumar Patel <jenu.16@icloud.com>
2018-09-08 02:58:33 -0500

    working with angular routers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,33 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/instructions.html'
+      })
+      .state('brick', {
+        url: '/bricks/:color',
+        templateUrl: 'views/bricks.html',
+        controllerProvider: function($stateParams) {
+          return $stateParams.color[0].toUpperCase() +
+            $stateParams.color.slice(1) +
+            'BricksCtrl';
+        },
+        controllerAs: 'brick'
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('brick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      });
+  }]);
